e3c07888e49dc036cc346c062692e579b4c04b75
logesh92z <logeshwaran090203@gmail.com>
2025-01-29 14:47:19 +0530

    Describe your changes

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -18,12 +18,14 @@ const Login = () => {
         type="email" 
         onChange={(e) => setEmail(e.target.value)} 
         value={email} 
+        placeholder="Staff id"
       />
       <label>Password:</label>
       <input 
         type="password" 
         onChange={(e) => setPassword(e.target.value)} 
         value={password} 
+        placeholder="Enter the Password"
       />
 
       <button disabled={isLoading}>Log in</button>
